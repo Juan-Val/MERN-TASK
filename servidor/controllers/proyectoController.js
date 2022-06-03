6bcbc49570cc58dd605b5c772cc7b9d85d1b0c19
Juan Valencia <juan.vo.11.2000@gmail.com>
2022-06-03 11:24:36 -0500

    proyecto conotroller

--- servidor/controllers/proyectoController.js
+++ servidor/controllers/proyectoController.js
@@ -1,6 +1,13 @@
 const Proyecto = require("../models/proyecto");
+const { validationResult } = require("express-validator");
 
 exports.crearProyecto = async (req, res) => {
+  // Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
   try {
     // Crear un nuevo proyecto
     const proyecto = new Proyecto(req.body);
@@ -11,6 +18,19 @@ exports.crearProyecto = async (req, res) => {
     // Guardar el proyecto
     proyecto.save();
     res.json(proyecto);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+// Obtener todos los proyectos
+exports.obtenerProyectos = async (req, res) => {
+  try {
+    const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({
+      creado: -1,
+    });
+    res.json({ proyectos });
   } catch (error) {
     console.log(error);
     res.status(500).send("Hubo un error");
--- servidor/routes/proyectos.js
+++ servidor/routes/proyectos.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const proyectoController = require("../controllers/proyectoController");
 const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 
 // Crea un usuarios
 // /api/proyectos
-router.post("/", [], auth, proyectoController.crearProyecto);
+router.post(
+  "/",
+  auth,
+  [check("nombre", "El nombre del proyecto es obligatorio").not().isEmpty()],
+  proyectoController.crearProyecto
+);
 
-router.get("/", [], auth, proyectoController.crearProyecto);
+router.get("/", auth, proyectoController.obtenerProyectos);
 
 module.exports = router;
